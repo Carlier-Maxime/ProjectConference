4ccac52736bd979d011c0f014490b7b00e3016f3
M_EtOuais <28m06c02@gmail.com>
2025-07-12 17:51:08 +0200

    add MyGameInstanceSubsystem with function for activatePluginByName

---
+++ Source/ProjectConference/MyGameInstanceSubsystem.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameInstanceSubsystem.h"
+
+#include "GameFeaturesSubsystem.h"
+#include "GameFeaturePluginOperationResult.h"
+
+void UMyGameInstanceSubsystem::ActivateGameFeatureByName(const FString& PluginName, bool ReActivate)
+{
+	FString URL;
+	if (!UGameFeaturesSubsystem::Get().GetPluginURLByName(PluginName, URL))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Plugin '%s' non trouvé parmi les plugins disponibles."), *PluginName);
+	}
+
+	if (ReActivate)
+	{
+		if (UGameFeaturesSubsystem::Get().IsGameFeaturePluginActive(URL))
+		{
+			UGameFeaturesSubsystem::Get().DeactivateGameFeaturePlugin(URL);
+		}
+
+	}
+	UGameFeaturesSubsystem::Get().LoadAndActivateGameFeaturePlugin(
+		URL,
+		FGameFeaturePluginLoadComplete::CreateStatic(&UMyGameInstanceSubsystem::OnPluginActivated)
+	);
+}
+
+void UMyGameInstanceSubsystem::OnPluginActivated(const UE::GameFeatures::FResult& Result)
+{
+	if (Result.HasError())
+	{
+		UE_LOG(LogTemp, Error, TEXT("❌ Échec activation plugin : %s"), *Result.GetError());	
+	}
+	else
+	{
+		UE_LOG(LogTemp, Display, TEXT("✅ Plugin activé avec succès !"));
+	}
+}
---
+++ Source/ProjectConference/MyGameInstanceSubsystem.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MyGameInstanceSubsystem.generated.h"
+
+namespace UE::GameFeatures
+{
+	struct FResult;
+}
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCONFERENCE_API UMyGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable)
+	static void ActivateGameFeatureByName(const FString& PluginName, bool ReActivate = false);
+	static void OnPluginActivated(const UE::GameFeatures::FResult& Result);
+};
