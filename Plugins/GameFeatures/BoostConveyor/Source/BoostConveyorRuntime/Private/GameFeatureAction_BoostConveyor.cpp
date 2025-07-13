bfa98b33e43dc1c045e3017eac51f33f7e9f34a7
M_EtOuais <28m06c02@gmail.com>
2025-07-13 17:41:50 +0200

    add BoostConveyor Game Features

---
+++ Plugins/GameFeatures/BoostConveyor/Source/BoostConveyorRuntime/Private/GameFeatureAction_BoostConveyor.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameFeatureAction_BoostConveyor.h"
+
+#include "ConveyorLogicComponent.h"
+
+void UGameFeatureAction_BoostConveyor::OnGameFeatureActivating(FGameFeatureActivatingContext& Context)
+{
+	Super::OnGameFeatureActivating(Context);
+	OldSpeed = UConveyorLogicComponent::GetCurrentSpeed();
+	UConveyorLogicComponent::SetCurrentSpeed(Speed);
+}
+
+void UGameFeatureAction_BoostConveyor::OnGameFeatureDeactivating(FGameFeatureDeactivatingContext& Context)
+{
+	UConveyorLogicComponent::SetCurrentSpeed(OldSpeed);
+	Super::OnGameFeatureDeactivating(Context);
+}
---
+++ Plugins/GameFeatures/BoostConveyor/Source/BoostConveyorRuntime/Public/GameFeatureAction_BoostConveyor.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFeatureAction.h"
+
+#include "GameFeatureAction_BoostConveyor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOOSTCONVEYORRUNTIME_API UGameFeatureAction_BoostConveyor : public UGameFeatureAction
+{
+	GENERATED_BODY()
+
+public:
+	virtual void OnGameFeatureActivating(FGameFeatureActivatingContext& Context) override;
+	virtual void OnGameFeatureDeactivating(FGameFeatureDeactivatingContext& Context) override;
+	UPROPERTY(EditAnywhere, Category = "BoostConveyor")
+	float Speed = 42;
+private:
+	float OldSpeed;
+};
